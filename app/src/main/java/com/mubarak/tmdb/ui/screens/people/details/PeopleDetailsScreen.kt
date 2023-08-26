87c0a5cc2192459bb6dfe7693a2701ce18a3eb15
Mubarak-1998 <mubarak.alqnaam@gmail.com>
2023-08-26 12:56:56 +0300

    - Add social media account for people

--- app/src/main/java/com/mubarak/tmdb/ui/screens/people/details/PeopleDetailsScreen.kt
+++ app/src/main/java/com/mubarak/tmdb/ui/screens/people/details/PeopleDetailsScreen.kt
@@ -3,8 +3,10 @@ package com.mubarak.tmdb.ui.screens.people.details
 import android.util.Log
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,6 +21,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,6 +31,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -112,57 +117,150 @@ fun PeopleDetailsScreen(
                 })
             }
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column(modifier = Modifier
-                .animateContentSize(animationSpec = tween(100))
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null
-                ) { showMore = !showMore }) {
-                if (showMore) {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        text = state?.data?.biography ?: ""
-                    )
-                } else {
-                    Text(
-                        text = state?.data?.biography ?: "",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        maxLines = 5,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                }
 
+        PeopleSocialMedia(peopleSocialMediaViewState)
+
+        Card(modifier = Modifier.fillMaxWidth().padding(4.dp)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column(modifier = Modifier
+                    .animateContentSize(animationSpec = tween(100))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) { showMore = !showMore }) {
+                    if (showMore) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            text = state?.data?.biography ?: ""
+                        )
+                    } else {
+                        Text(
+                            text = state?.data?.biography ?: "",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            maxLines = 5,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
             }
         }
 
-        LazyRow(content = {
-            items(peopleMovieCreditsViewState?.data.orEmpty()) { item ->
-                MovieCard(
-                    item.title ?: item.originalTitle ?: "-",
-                    item.posterPath,
-                    modifier = Modifier.clickable {
-                        navigator.navigate(
-                            DetailsScreenDestination(
-                                id = 2,
-                                movieId = item.id,
-                                movieTitle = item.title ?: item.originalTitle,
-                                pathType = "movie",
-                                )
-                        )
-                    })
-            }
-        })
 
-        LazyRow(content = {
-            items(imagesState?.data.orEmpty()) { item ->
-                PeopleImages(item?.filePath)
-            }
-        })
+        MovieCreditsLazyRow(peopleMovieCreditsViewState, navigator)
+        PersonImagesLazyRow(imagesState)
 
     }
+}
+
+@Composable
+fun PeopleSocialMedia(peopleSocialMediaViewState: PeopleSocialMediaViewState?) {
+    val uriHandler = LocalUriHandler.current
+
+    Card(modifier = Modifier.padding(4.dp).fillMaxWidth()) {
+        Row (horizontalArrangement = Arrangement.Center){
+            if (!peopleSocialMediaViewState?.data?.imdbId.isNullOrEmpty()) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_imdb),
+                    contentDescription = "imdb",
+                    modifier = Modifier
+                        .clickable {
+                            uriHandler.openUri("https://www.imdb.com/name/" + peopleSocialMediaViewState?.data?.imdbId)
+                        }
+                        .padding(8.dp)
+                )
+            }
+
+
+            if (!peopleSocialMediaViewState?.data?.instagramId.isNullOrEmpty()) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_instagram),
+                    contentDescription = "imdb",
+                    modifier = Modifier
+                        .clickable {
+                            uriHandler.openUri("https://www.instagram.com/" + peopleSocialMediaViewState?.data?.instagramId)
+                        }
+                        .padding(8.dp)
+                        .height(28.dp)
+                        .width(54.dp)
+                )
+            }
+
+            if (!peopleSocialMediaViewState?.data?.tiktokId.isNullOrEmpty()) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_tiktok),
+                    contentDescription = "imdb",
+                    modifier = Modifier
+                        .clickable {
+                            uriHandler.openUri("https://www.tiktok.com/@" + peopleSocialMediaViewState?.data?.tiktokId)
+                        }
+                        .padding(8.dp)
+                        .height(28.dp)
+                        .width(54.dp)
+                )
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_twitter_x),
+                contentDescription = "imdb",
+                modifier = Modifier
+                    .clickable {
+                        uriHandler.openUri("https://twitter.com/" + peopleSocialMediaViewState?.data?.twitterId)
+                    }
+                    .padding(8.dp)
+                    .height(28.dp)
+                    .width(54.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_youtube),
+                contentDescription = "imdb",
+                modifier = Modifier
+                    .clickable {
+                        uriHandler.openUri("https://www.youtube.com/user/" + peopleSocialMediaViewState?.data?.youtubeId)
+                    }
+                    .padding(8.dp)
+                    .height(28.dp)
+                    .width(54.dp)
+            )
+
+        }
+    }
+}
+
+@Composable
+private fun PersonImagesLazyRow(imagesState: PeopleImagesViewState?) {
+    LazyRow(content = {
+        items(imagesState?.data.orEmpty()) { item ->
+            PeopleImages(item?.filePath)
+        }
+    })
+}
+
+@Composable
+private fun MovieCreditsLazyRow(
+    peopleMovieCreditsViewState: PeopleMovieCreditsViewState?,
+    navigator: DestinationsNavigator
+) {
+    LazyRow(content = {
+        items(peopleMovieCreditsViewState?.data.orEmpty()) { item ->
+            MovieCard(
+                item.title ?: item.originalTitle ?: "-",
+                item.posterPath,
+                modifier = Modifier.clickable {
+                    navigator.navigate(
+                        DetailsScreenDestination(
+                            id = 2,
+                            movieId = item.id,
+                            movieTitle = item.title ?: item.originalTitle,
+                            pathType = "movie",
+                        )
+                    )
+                })
+        }
+    })
 }
